11f3f8e178723011f08e3879382709c07cf3958c
Rafael Araujo <kasim.85.ra@gmail.com>
2023-09-17 18:28:07 -0300

    CRUD

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models import db, User
 
 #Se instancia nuesta aPP en Flask
@@ -8,9 +8,67 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///proyect.db"
 db.init_app(app) #coneccion a las base de datos al ajecutar app
 
 
+@app.route("/")
+def home():
+    return "<h1>Hola desde flask<h1/>"
 
 
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    #===INSTANCIA DE LA TABLA
+    user = User()   #crear instancia
+ #=== CAPTURA DE DATA   
+    data = request.get_json()
+    user.name = data["name"]
+    user.password = data["password"]
+    user.username = data["username"]
 
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "msj": "User created",
+        "status": "success"
+    }), 200
+
+@app.route("/update_user/<int:id>", methods=["PUT"]) #===indicando actualizar por el ID==
+def update_user(id):
+    user = User.query.get(id)
+    if user is not None:
+        data = request.get_json()
+        user.name = data["name"]
+
+        db.session.commit()
+
+        return jsonify({
+        "msj": "User updated",
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "User not found",
+        "status": "error"
+    }),404
+
+@app.route("/delete_user/<int:id>", methods=["DELETE"])
+def delete_user(id):
+    user = User.query.get(id)
+    if user is not None:
+        db.session.delete(user)
+        db.session.commit()
+
+        return jsonify({
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "User not found",
+        "status": "error"
+    }), 404
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":  
-    app.run(host="localhost", port=5000)
+    app.run(host="localhost", port=8080)
