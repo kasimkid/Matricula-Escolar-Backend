c59874d6123c19cbc3d147b92562e2bb2c53189c
Rafael Araujo <kasim.85.ra@gmail.com>
2023-09-21 19:55:43 -0300

    changes letters

--- models.py
+++ models.py
@@ -1,9 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 
-
 db = SQLAlchemy()
-
 
 class Student(db.Model):
     __tablename__ = 'student'
