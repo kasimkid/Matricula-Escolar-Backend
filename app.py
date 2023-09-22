3b1a2a55433d6fea3636543ec581926054180bd2
SrMetus <s.gaggero87@gmail.com>
2023-09-22 19:51:09 -0300

    endpoint created

--- app.py
+++ app.py
@@ -1,54 +1,192 @@
 from flask import Flask, request, jsonify
-from models import db, Administrator
+from models import db, Student, Apfinancial, Apacademic, Administrator, Grade, Course, Status, Roll
+from flask_migrate import Migrate
 
 #Se instancia nuesta aPP en Flask
 app = Flask(__name__)
 # print("nombre del archivo",__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///proyect.db"
 db.init_app(app) #coneccion a las base de datos al ajecutar app
-
+migrate = Migrate(app, db)
 
 @app.route("/")
 def home():
     return "<h1>Probando flask<h1/>"
 
 
-@app.route("/create_user", methods=["POST"])
-def create_user():
+@app.route("/create_account", methods=["POST"])
+def create_account():
     #===INSTANCIA DE LA TABLA
     user = Administrator()   #crear instancia
- #=== CAPTURA DE DATA   
-    data = request.get_json()
-    user.rut = data["rut"]
-    user.password = data["password"]
-    user.name = data["name"]
-    user.last_name = data["last_name"]
-    user.email = data["email"]
+    #=== CAPTURA DE DATA
+    if user is not None:   
+        data = request.get_json()
+        user.rut = data["rut"]
+        user.password = data["password"]
+        user.email_student = data["email_student"]
 
-    db.session.add(user)
-    db.session.commit()
 
-    return jsonify({
-        "msj": "User created",
-        "status": "success"
-    }), 200
+        db.session.add(user)
+        db.session.commit()
 
-@app.route("/update_user/<int:id>", methods=["PUT"]) #===indicando actualizar por el ID==
-def update_user(id):
-    user = Administrator.query.get(id)
+        return jsonify({
+            "msj": "Student created",
+            "status": "success"
+        }), 200
+    else:
+        return jsonify({
+            'msj': 'Student not created',
+            'status': "Error"
+        }), 404
+
+@app.route("/update_student", methods=["POST"]) #llenar datos de estudiante
+def update_student():
+    user = Student()
     if user is not None:
         data = request.get_json()
+        user.password = data["password"]
         user.name = data["name"]
+        user.last_name = data["last_name"]
+        user.gender = data["gender"]
+        user.birthday = data["birthday"]
+        user.address = data["address"]
+        user.health_system = data["health_system"]
+        user.observation = data["observation"]
 
         db.session.commit()
 
         return jsonify({
-        "msj": "User updated",
+        "msj": "Student updated",
         "status": "success"
     }), 201
     else:
         return jsonify({
-        "msj": "User not found",
+        "msj": "not found",
+        "status": "error"
+    }), 404
+
+@app.route("/edit_student/<int:id>", methods=["PUT"]) #===indicando actualizar por el ID==
+def edit_student(id):
+    user = Student.query.get(id)
+    if user is not None:
+        data = request.get_json()
+        #user.rut_student = data["rut_student"]
+        user.password = data["password"]
+        user.name = data["name"]
+        user.last_name = data["last_name"]
+        user.gender = data["gender"]
+        user.birthday = data["birthday"]
+        user.address = data["address"]
+        #user.email_student = data["email_student"]
+        user.health_system = data["health_system"]
+        user.observation = data["observation"]
+        #user.course = data["course"]
+
+        db.session.commit()
+
+        return jsonify({
+        "msj": "Student updated",
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "Student not found",
+        "status": "error"
+    }), 404
+
+@app.route("/update_financial", methods=["POST"])
+def update_financial():
+    user = Apfinancial()
+    if user is not None:
+        data = request.get_json()
+        user.rut_financial = data["rut_financial"]
+        user.name = data["name"]
+        user.last_name = data["last_name"]
+        user.contact_number = data["contact_number"]
+        user.address = data["address"]
+        user.email_financial = data["email_student"]
+
+        db.session.commit()
+
+        return jsonify({
+        "msj": "Financial updated",
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "Financial not found",
+        "status": "error"
+    }),404
+
+@app.route("/edit_financial/<int:id>", methods=["PUT"]) #===indicando actualizar por el ID==
+def edit_financial(id):
+    user = Apfinancial.query.get(id)
+    if user is not None:
+        data = request.get_json()
+        user.rut_financial = data["rut_financial"]
+        user.name = data["name"]
+        user.last_name = data["last_name"]
+        user.contact_number = data["contact_number"]
+        user.address = data["address"]
+        user.email_financial = data["email_student"]
+
+        db.session.commit()
+
+        return jsonify({
+        "msj": "Financial updated",
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "Financial not found",
+        "status": "error"
+    }),404
+
+@app.route("/update_academic", methods=["POST"])
+def update_academic():
+    user = Apacademic()
+    if user is not None:
+        data = request.get_json()
+        user.rut_financial = data["rut_financial"]
+        user.name = data["name"]
+        user.last_name = data["last_name"]
+        user.contact_number = data["contact_number"]
+        user.address = data["address"]
+        user.email_financial = data["email_student"]
+
+        db.session.commit()
+
+        return jsonify({
+        "msj": "Academic updated",
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "Academic not found",
+        "status": "error"
+    }),404
+
+@app.route("/edit_academic/<int:id>", methods=["PUT"]) #===indicando actualizar por el ID==
+def edit_academic(id):
+    user = Apacademic.query.get(id)
+    if user is not None:
+        data = request.get_json()
+        user.rut_financial = data["rut_financial"]
+        user.name = data["name"]
+        user.last_name = data["last_name"]
+        user.contact_number = data["contact_number"]
+        user.address = data["address"]
+        user.email_financial = data["email_student"]
+
+        db.session.commit()
+
+        return jsonify({
+        "msj": "Academic updated",
+        "status": "success"
+    }), 201
+    else:
+        return jsonify({
+        "msj": "Academic not found",
         "status": "error"
     }),404
 
@@ -67,6 +205,19 @@ def delete_user(id):
         "msj": "User not found",
         "status": "error"
     }), 404
+
+######################## GET POINT ###############################
+@app.route('/students')
+def list_student():
+    students = Student.query.with_entities(Student.rut_student, Student.name, Student.last_name).all()
+    result_students = [{"rut": rut, "name": nombre, "last_name": apellido} for rut, nombre, apellido in students]
+    return jsonify(result_students)
+
+@app.route('/courses')
+def list_course():
+    courses = Course.query.with_entities(Course.course).all()
+    result_courses = [{"course": course} for course in courses]
+    return jsonify(result_courses)
 
 if __name__ == "__main__":  
     app.run(host="localhost", port=8080)
--- models.py
+++ models.py
@@ -5,96 +5,90 @@ db = SQLAlchemy()
 class Student(db.Model):
     __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key=True)
-    rut = db.Column(db.Integer, unique=True, nullable=False)
-    password = db.Column(db.String(16))
+    rut_student = db.Column(db.String(12), unique=True, nullable=False)
+    password = db.Column(db.String(16), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     gender = db.Column(db.String(15), nullable=False)
-    birthday = db.Column(db.Date, nullable=False)
+    birthday = db.Column(db.DateTime, nullable=False)
     address = db.Column(db.String(250), nullable=False)
-    email = db.Column(db.String(60), unique=True, nullable=False)
+    email_student = db.Column(db.String(60), unique=True, nullable=False)
     health_system = db.Column(db.String(25), nullable=False)
     observation = db.Column(db.String(250), nullable=True)
-
-    # relations
-    financial = db.relationship('Ap_Financial', uselist=False, back_populates='student')
-    academic = db.relationship('Ap_Academic', uselist=False, back_populates='student')
-
+    #course_id =db.Column(db.Integer)
+    #relationship
+    financial = db.relationship('Apfinancial')
+    academic = db.relationship('Apacademic')
+    grades = db.relationship('Grade')
+    course = db.relationship('Course')
+    status = db.relationship('Status')
+    roll = db.relationship('Roll')
+    def serialize(self):
+        return {
+            "id": self.id,
+            "rut": self.rut_student,
+            "name": self.name,
+            "last_name": self.last_name,
+            "gender": self.gender,
+            "birthday": self.birthday,
+            "adrress": self.adrress,
+            "email": self.email_student,
+            "health_system": self.health_system,
+            "observation": self.observation
+        }
+class Apfinancial(db.Model):
+    __tablename__ = 'apfinancial'
+    id = db.Column(db.Integer, primary_key=True)
+    rut_financial = db.Column(db.String(12), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    contact_number = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    email_financial = db.Column(db.String(60), unique=True, nullable=False)
+    # Foreign Key
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    def serialize(self):
+        return {
+            "id": self.id,
+            "rut": self.rut_financial,
+            "name": self.name,
+            "last_name": self.last_name,
+            "contact_number": self.contact_number,
+            "address": self.address,
+            "email": self.email_financial
+        }
+class Apacademic(db.Model):
+    __tablename__ = 'apacademic'
+    id = db.Column(db.Integer, primary_key=True)
+    rut = db.Column(db.String(12), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    contact_number = db.Column(db.Integer, nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+     # Foreign Key
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
     def serialize(self):
         return {
             "id": self.id,
             "rut": self.rut,
             "name": self.name,
             "last_name": self.last_name,
-            "gender": self.gender,
-            "birthdaadrress": self.birthdaadrress,
-            "adrress": self.adrress,
-            "email": self.email,
-            "health_system": self.health_system,
-            "observation": self.observation
-        }
-
-
-class Ap_Financial(db.Model):
-    __tablename__ = 'ap_financial'
-    id = db.Column(db.Integer, primary_key=True)
-    rut = db.Column(db.Integer, unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    contact_number = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(60), unique=True, nullable=False)
-
-    # Foreign Key
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "runame": self.runame,
-            "name": self.name,
-            "last_name": self.last_name,
             "contact_number": self.contact_number,
             "address": self.address,
             "email": self.email
         }
-
-
-class Ap_Academic(db.Model):
-    __tablename__ = 'ap_academic'
-    id = db.Column(db.Integer, primary_key=True)
-    rut = db.Column(db.Integer, unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    contact_number = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(60), unique=True, nullable=False)
-    student = db.relationship('Student', back_populates='ap_academic')
-
-    # Foreign Key
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "runame": self.runame,
-            "name": self.name,
-            "last_name": self.last_name,
-            "contact_number": self.contact_number,
-            "address": self.address,
-            "email": self.email
-        }
-
-
 class Administrator(db.Model):
     __tablename__ = 'administrator'
     id = db.Column(db.Integer, primary_key=True)
-    rut = db.Column(db.Integer, unique=True, nullable=False)
+    rut = db.Column(db.String(12), unique=True, nullable=False)
     password = db.Column(db.String(16), nullable=False)
     name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(60), unique=True, nullable=False)
-
+    # relationship
+    roll = db.relationship('Roll')
+    status = db.relationship('Status')
     def serialize(self):
         return {
             "id": self.id,
@@ -104,51 +98,51 @@ class Administrator(db.Model):
             "last_name": self.last_name,
             "email": self.email
         }
-
-
 class Grade(db.Model):
     __tablename__ = 'grade'
     id = db.Column(db.Integer, primary_key=True)
     grade = db.Column(db.String(20), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-
+    date = db.Column(db.DateTime, nullable=False)
+    # Foreign Key
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    # relationship
+    student = db.relationship('Student')
     def serialize(self):
         return {
             "id": self.id,
             "grade": self.grade,
             "date": self.date
         }
-
-
 class Course(db.Model):
     __tablename__ = 'course'
     id = db.Column(db.Integer, primary_key=True)
-    course = db.Column(db.String, nullable=False)
-
+    course = db.Column(db.String(20), nullable=False)
+    # Foreign Key
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
     def serialize(self):
         return {
             "id": self.id,
             "course": self.course
         }
-
-
 class Status(db.Model):
     __tablename__ = 'status'
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.Integer, nullable=False)
-
+    # Foreign Key
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    administrator_id = db.Column(db.Integer, db.ForeignKey('administrator.id'))
     def serialize(self):
         return {
             "id": self.id,
             "status": self.status
         }
-
-
 class Roll(db.Model):
     __tablename__ = 'roll'
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.Integer, nullable=False)
-
+    # Foreign Key
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    administrator_id = db.Column(db.Integer, db.ForeignKey('administrator.id'))
     def serialize(self):
         return {
             "id": self.id,
